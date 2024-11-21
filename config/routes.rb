c4a8ae5efafa7d96155561c65be8bba6b0ed7078
ksk-n <k.021759@gmail.com>
2024-11-21 21:28:01 +0900

    scaffoldコマンドで作成したsample_article関連のファイルを全て削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get '/mypage', to: 'mypage#show'
   devise_for :users
   root to: 'articles#index'
-  resources :sample_articles
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
